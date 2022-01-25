7d6e7b3edae07227ee706ab59a9d08f4582e51d4
zhrxxxx <77419993+zhrxxxx@users.noreply.github.com>
2022-01-24 23:07:40 -0500

    Add files via upload

---
+++ day14/background.js
@@ -0,0 +1,11 @@
+﻿function reddenPage() {
+
+    document.body.style.backgroundColor = 'black';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+    });
+});
---
+++ day14/popup.js
@@ -0,0 +1,5 @@
+﻿let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+    changeColor.style.backgroundColor = 'black';
+})
