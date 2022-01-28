2e0eaa177a2715fc496ec480954158351c14405a
zhrxxxx <77419993+zhrxxxx@users.noreply.github.com>
2022-01-28 12:08:54 -0500

    Add files via upload

---
+++ day18/background.js
@@ -0,0 +1,10 @@
+﻿function reddenPage() {
+    document.body.style.backgroundColor = 'red';
+}
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: reddenPage
+    });
+});
